749d594d9db1b622ab7607895595fea2937590c2
Kentaro Kuribayashi <kentarok@gmail.com>
2013-12-11 15:31:37 +0900

    Modify setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# gom: https://github.com/mattn/gom
-gom install
-gom build -o delta delta.go
+go get -u github.com/kentaro/delta
+go build -o delta delta.go
 
 bundle install --path=vendor/bundle
 carton install
@@ -10,10 +9,14 @@ carton install
 # Build ab because `/usr/sbin/ab` (Version 2.3 <$Revision: 655654 $>) bundled with Mac OS X is broken
 # http://adventuresincoding.com/2012/05/how-to-get-apachebenchab-to-work-on-mac-os-x-lion
 brew upgrade pcre
+
 if [ ! -e httpd-2.4.7.tar.gz ]; then
   wget http://ftp.tsukuba.wide.ad.jp/software/apache/httpd/httpd-2.4.7.tar.gz
+  tar zxvf httpd-2.4.7.tar.gz
 fi
-tar zxvf httpd-2.4.7.tar.gz
-cd httpd-2.4.7/
-./configure
-make
+
+if [ ! -e httpd-2.4.7/support/ab ]; then
+  cd httpd-2.4.7/
+  ./configure
+  make
+fi
